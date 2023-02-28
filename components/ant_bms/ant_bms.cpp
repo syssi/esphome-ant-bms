a84fa3d2db8dfa70ddf8bd6035999744c8b241f5
Sebastian Muszynski <basti@linkt.de>
2023-02-28 09:07:49 +0100

    Simplify component by removing ant_modbus

--- components/ant_bms/ant_bms.cpp
+++ components/ant_bms/ant_bms.cpp
@@ -66,7 +66,95 @@ static const char *const BALANCER_STATUS[BALANCER_STATUS_SIZE] = {
     "Motherboard over temperature",          // 0x0A
 };
 
-void AntBms::on_ant_modbus_data(const uint8_t &function, const std::vector<uint8_t> &data) {
+void AntBms::loop() {
+  const uint32_t now = millis();
+  if (now - this->last_byte_ > this->rx_timeout_) {
+    this->rx_buffer_.clear();
+    this->last_byte_ = now;
+  }
+
+  while (this->available()) {
+    uint8_t byte;
+    this->read_byte(&byte);
+    if (this->parse_ant_bms_byte_(byte)) {
+      this->last_byte_ = now;
+    } else {
+      this->rx_buffer_.clear();
+    }
+  }
+}
+
+bool AntBms::parse_ant_bms_byte_(uint8_t byte) {
+  size_t at = this->rx_buffer_.size();
+  this->rx_buffer_.push_back(byte);
+  const uint8_t *raw = &this->rx_buffer_[0];
+  uint16_t frame_len = 140;
+
+  // 0xAA 0x55 0xAA 0xFF 0x02 0x1D 0x10 0x2A ...
+  //   0    1    2    3  ^^^^^ data ^^^^^^^^
+
+  // Byte 0: Ant BMS start byte (match all)
+  if (at == 0)
+    return true;
+  uint8_t address = raw[0];
+
+  // Byte 0...3: Header
+  if (at < 4)
+    return true;
+
+  if (!(raw[0] == 0xAA && raw[1] == 0x55 && raw[2] == 0xAA && raw[3] == 0xFF) && !(raw[0] == 0x7E && raw[1] == 0xA1)) {
+    ESP_LOGW(TAG, "Invalid header");
+
+    // return false to reset buffer
+    return false;
+  }
+
+  // Byte 0...5
+  if (at < 6)
+    return true;
+
+  // Calculate new protocol frame length
+  if (address == 0x7E) {
+    frame_len = 6 + raw[5] + 4;
+  }
+
+  // Byte 0...139
+  if (at < frame_len - 1)
+    return true;
+
+  // New protocol response frame
+  //
+  // 0x7E 0xA1 0x43 0x6A 0x01 0x02 0x05 0x00 0x1E 0x5C 0xAA 0x55    Password ACK
+  // 0x7E 0xA1 0x61 0x04 0x00 0x02 0x01 0x00 0xF2 0x2B 0xAA 0x55    Register 0x04 ACK
+  // 0x7E 0xA1 0x61 0x06 0x00 0x02 0x01 0x00 0x8B 0xEB 0xAA 0x55    Register 0x06 ACK
+  //  0    1    2    3    4    5    6    7    8    9    10   11
+  // head add  func val  val  len  data data crc  crc  end  end
+  if (address == 0x7E) {
+    // Discard new protocol frame for now
+    ESP_LOGVV(TAG, "New protocol response frame discarded: %s", format_hex_pretty(raw, at + 1).c_str());
+    return false;
+  }
+
+  uint8_t function = raw[3];
+
+  uint16_t computed_crc = chksum_(raw, frame_len - 2);
+  uint16_t remote_crc = uint16_t(raw[frame_len - 2]) << 8 | (uint16_t(raw[frame_len - 1]) << 0);
+  if (computed_crc != remote_crc) {
+    ESP_LOGW(TAG, "CRC Check failed! %04X != %04X", computed_crc, remote_crc);
+    return false;
+  }
+
+  ESP_LOGVV(TAG, "RX <- %s", format_hex_pretty(raw, at + 1).c_str());
+
+  std::vector<uint8_t> data(this->rx_buffer_.begin(), this->rx_buffer_.begin() + frame_len);
+
+  this->on_ant_bms_data_(function, data);
+
+  // return false to reset buffer
+  return false;
+}
+
+void AntBms::on_ant_bms_data_(const uint8_t &function, const std::vector<uint8_t> &data) {
   if (data.size() == 140) {
     this->on_status_data_(data);
     return;
@@ -199,10 +287,10 @@ void AntBms::on_status_data_(const std::vector<uint8_t> &data) {
 }
 
 void AntBms::update() {
-  this->read_registers();
+  this->read_registers_();
 
   if (this->enable_fake_traffic_) {
-    this->on_ant_modbus_data(
+    this->on_ant_bms_data_(
         FUNCTION_READ_ALL,
         {
             0xAA, 0x55, 0xAA, 0xFF, 0x01, 0x0B, 0x0D, 0x0C, 0x0D, 0x0C, 0x0D, 0x0B, 0x0D, 0x0A, 0x0D, 0x0C, 0x0D, 0x0B,
@@ -219,13 +307,13 @@ void AntBms::update() {
 
 void AntBms::write_register(uint8_t address, uint16_t value) {
   if (this->supports_new_commands_) {
-    this->send_v2021(0x51, address, value);
+    this->send_v2021_(0x51, address, value);
     return;
   }
 
   this->authenticate_();
-  this->send(WRITE_SINGLE_REGISTER, address, value);
-  this->send(WRITE_SINGLE_REGISTER, REGISTER_APPLY_WRITE, 0x00);
+  this->send_(WRITE_SINGLE_REGISTER, address, value);
+  this->send_(WRITE_SINGLE_REGISTER, REGISTER_APPLY_WRITE, 0x00);
 }
 
 void AntBms::authenticate_() {
@@ -237,7 +325,7 @@ void AntBms::authenticate_() {
   ESP_LOGD(TAG, "Authenticate write command");
   const uint8_t *password = reinterpret_cast<const uint8_t *>(this->password_.c_str());
   for (uint8_t i = 0; i < 4; i++) {
-    this->send(WRITE_SINGLE_REGISTER, 0xF1 + i, (uint16_t(password[i]) << 8) | uint16_t(password[i + 1]));
+    this->send_(WRITE_SINGLE_REGISTER, 0xF1 + i, (uint16_t(password[i]) << 8) | uint16_t(password[i + 1]));
   }
 }
 
@@ -264,7 +352,8 @@ void AntBms::publish_state_(text_sensor::TextSensor *text_sensor, const std::str
 
 void AntBms::dump_config() {  // NOLINT(google-readability-function-size,readability-function-size)
   ESP_LOGCONFIG(TAG, "AntBms:");
-  ESP_LOGCONFIG(TAG, "  Address: 0x%02X", this->address_);
+  ESP_LOGCONFIG(TAG, "  RX timeout: %d ms", this->rx_timeout_);
+  ESP_LOGCONFIG(TAG, "  Fake traffic enabled: %s", YESNO(this->enable_fake_traffic_));
 
   LOG_SENSOR("", "Battery Strings", this->battery_strings_sensor_);
   LOG_SENSOR("", "Total Voltage", this->total_voltage_sensor_);
@@ -319,12 +408,113 @@ void AntBms::dump_config() {  // NOLINT(google-readability-function-size,readabi
   LOG_SENSOR("", "Cell Voltage 31", this->cells_[30].cell_voltage_sensor_);
   LOG_SENSOR("", "Cell Voltage 32", this->cells_[31].cell_voltage_sensor_);
   LOG_SENSOR("", "Charge Mosfet Status Code", this->charge_mosfet_status_code_sensor_);
-  LOG_TEXT_SENSOR("", "Charge Mosfet Status", this->charge_mosfet_status_text_sensor_);
   LOG_SENSOR("", "Discharge Mosfet Status Code", this->discharge_mosfet_status_code_sensor_);
-  LOG_TEXT_SENSOR("", "Discharge Mosfet Status", this->discharge_mosfet_status_text_sensor_);
   LOG_SENSOR("", "Balancer Status Code", this->balancer_status_code_sensor_);
+
+  LOG_TEXT_SENSOR("", "Discharge Mosfet Status", this->discharge_mosfet_status_text_sensor_);
+  LOG_TEXT_SENSOR("", "Charge Mosfet Status", this->charge_mosfet_status_text_sensor_);
   LOG_TEXT_SENSOR("", "Balancer Status", this->balancer_status_text_sensor_);
   LOG_TEXT_SENSOR("", "Total Runtime Formatted", this->total_runtime_formatted_text_sensor_);
+}
+
+float AntBms::get_setup_priority() const {
+  // After UART bus
+  return setup_priority::BUS - 1.0f;
+}
+
+void AntBms::send_(uint8_t function, uint8_t address, uint16_t value) {
+  uint8_t frame[6];
+  frame[0] = 0xA5;
+  frame[1] = function;    // 0xA5
+  frame[2] = address;     // 0xFA (Charge MOSFET)
+  frame[3] = value >> 8;  // 0x00
+  frame[4] = value >> 0;  // 0x01 (On)
+  frame[5] = frame[2] + frame[3] + frame[4];
+
+  this->write_array(frame, 6);
+  this->flush();
+}
+
+void AntBms::read_registers_() {
+  uint8_t frame[6];
+  frame[0] = 0x5A;
+  frame[1] = 0x5A;
+  frame[2] = 0x00;
+  frame[3] = 0x00;
+  frame[4] = 0x01;
+  frame[5] = 0x01;
+
+  this->write_array(frame, 6);
+  this->flush();
+}
+
+void AntBms::authenticate_v2021_() {
+  uint8_t frame[22];
+
+  frame[0] = 0x7e;  // header
+  frame[1] = 0xa1;  // header
+  frame[2] = 0x23;  // control
+  frame[3] = 0x6a;  // address
+  frame[4] = 0x01;
+  frame[5] = 0x0c;   // data len
+  frame[6] = 0x31;   // 1
+  frame[7] = 0x32;   // 2
+  frame[8] = 0x33;   // 3
+  frame[9] = 0x34;   // 4
+  frame[10] = 0x35;  // 5
+  frame[11] = 0x36;  // 6
+  frame[12] = 0x37;  // 7
+  frame[13] = 0x38;  // 8
+  frame[14] = 0x39;  // 9
+  frame[15] = 0x61;  // a
+  frame[16] = 0x62;  // b
+  frame[17] = 0x63;  // c
+
+  auto crc = crc16_(frame + 1, 17);
+  frame[18] = crc >> 0;  // CRC
+  frame[19] = crc >> 8;  // CRC
+
+  frame[20] = 0xaa;
+  frame[21] = 0x55;
+
+  this->write_array(frame, 22);
+  this->flush();
+}
+
+void AntBms::authenticate_v2021_variable_(const uint8_t *data, uint8_t data_len) {
+  std::vector<uint8_t> frame = {0x7E, 0xA1, 0x23, 0x6A, 0x01};
+  frame.push_back(data_len);
+  for (int i = 0; i < data_len; i++) {
+    frame.push_back(data[i]);
+  }
+  auto crc = crc16_(frame.data() + 1, frame.size());
+  frame.push_back(crc >> 0);
+  frame.push_back(crc >> 8);
+  frame.push_back(0xAA);
+  frame.push_back(0x55);
+
+  this->write_array(frame.data(), frame.size());
+  this->flush();
+}
+
+void AntBms::send_v2021_(uint8_t function, uint8_t address, uint16_t value) {
+  this->authenticate_v2021_();
+
+  uint8_t frame[10];
+  frame[0] = 0x7e;        // header
+  frame[1] = 0xa1;        // header
+  frame[2] = function;    // control
+  frame[3] = address;     // address
+  frame[4] = value >> 8;  // value
+  frame[5] = value >> 0;  // value
+  auto crc = crc16_(frame + 1, 5);
+  frame[6] = crc >> 0;  // CRC
+  frame[7] = crc >> 8;  // CRC
+  frame[8] = 0xaa;      // footer
+  frame[9] = 0x55;      // footer
+
+  this->write_array(frame, 10);
+  this->flush();
 }
 
 }  // namespace ant_bms
