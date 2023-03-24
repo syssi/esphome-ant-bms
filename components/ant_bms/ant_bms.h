6875d4b05a76d9a1a2a40771d4e4fef447fb1c30
Sebastian Muszynski <basti@linkt.de>
2023-03-24 07:51:30 +0100

    Move fake traffic

--- components/ant_bms/ant_bms.h
+++ components/ant_bms/ant_bms.h
@@ -89,7 +89,6 @@ class AntBms : public uart::UARTDevice, public PollingComponent {
   void set_bluetooth_switch(switch_::Switch *bluetooth_switch) { bluetooth_switch_ = bluetooth_switch; }
   void set_buzzer_switch(switch_::Switch *buzzer_switch) { buzzer_switch_ = buzzer_switch; }
 
-  void set_enable_fake_traffic(bool enable_fake_traffic) { enable_fake_traffic_ = enable_fake_traffic; }
   void set_password(const std::string &password) { this->password_ = password; }
   void set_supports_new_commands(bool supports_new_commands) { supports_new_commands_ = supports_new_commands; }
 
@@ -137,7 +136,6 @@ class AntBms : public uart::UARTDevice, public PollingComponent {
     sensor::Sensor *temperature_sensor_{nullptr};
   } temperatures_[6];
 
-  bool enable_fake_traffic_;
   bool supports_new_commands_;
   std::string password_;
 
@@ -146,7 +144,7 @@ class AntBms : public uart::UARTDevice, public PollingComponent {
   uint32_t last_byte_{0};
   uint16_t rx_timeout_{50};
 
-  void on_ant_bms_data_(const uint8_t &function, const std::vector<uint8_t> &data);
+  void on_ant_bms_data_(const std::vector<uint8_t> &data);
   void on_status_data_(const std::vector<uint8_t> &data);
   bool parse_ant_bms_byte_(uint8_t byte);
   void authenticate_();
