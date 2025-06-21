9ab38d68621df15319b64d40b8a2e2e1783ec94b
Sebastian Muszynski <basti@linkt.de>
2025-06-21 20:44:35 +0200

    Fix

--- components/ant_bms_ble/switch/__init__.py
+++ components/ant_bms_ble/switch/__init__.py
@@ -1,10 +1,10 @@
 import esphome.codegen as cg
 from esphome.components import switch
 import esphome.config_validation as cv
-from esphome.const import CONF_ID
+from esphome.const import CONF_BLUETOOTH, CONF_ID
 
 from .. import CONF_ANT_BMS_BLE_ID, AntBmsBle, ant_bms_ble_ns
-from ..const import CONF_BALANCER, CONF_BLUETOOTH, CONF_CHARGING, CONF_DISCHARGING
+from ..const import CONF_BALANCER, CONF_CHARGING, CONF_DISCHARGING
 
 DEPENDENCIES = ["ant_bms_ble"]
 
