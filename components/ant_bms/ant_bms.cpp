4d51c7ff8823e35d81df7b6699b8e548fff9b8fd
Sebastian Muszynski <basti@linkt.de>
2025-07-11 19:28:16 +0200

    Complete dump config (#116)

--- components/ant_bms/ant_bms.cpp
+++ components/ant_bms/ant_bms.cpp
@@ -388,6 +388,7 @@ void AntBms::dump_config() {  // NOLINT(google-readability-function-size,readabi
   LOG_SENSOR("", "Maximum cell voltage", this->max_cell_voltage_sensor_);
   LOG_SENSOR("", "Min Voltage Cell", this->min_voltage_cell_sensor_);
   LOG_SENSOR("", "Max Voltage Cell", this->max_voltage_cell_sensor_);
+  LOG_SENSOR("", "Delta Cell Voltage", this->delta_cell_voltage_sensor_);
   LOG_SENSOR("", "Temperature 1", this->temperatures_[0].temperature_sensor_);
   LOG_SENSOR("", "Temperature 2", this->temperatures_[1].temperature_sensor_);
   LOG_SENSOR("", "Temperature 3", this->temperatures_[2].temperature_sensor_);
