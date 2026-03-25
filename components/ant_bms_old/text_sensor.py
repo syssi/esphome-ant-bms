9f9267c5b95e0734ab1dafb54d1e75207ebd1c50
Sebastian Muszynski <basti@linkt.de>
2026-03-25 11:02:19 +0100

    Use new_text_sensor() instead of deprecated register_text_sensor (#129)

--- components/ant_bms_old/text_sensor.py
+++ components/ant_bms_old/text_sensor.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 from esphome.components import text_sensor
 import esphome.config_validation as cv
-from esphome.const import CONF_ID, ICON_TIMELAPSE
+from esphome.const import ICON_TIMELAPSE
 
 from . import ANT_BMS_OLD_COMPONENT_SCHEMA, CONF_ANT_BMS_OLD_ID
 
@@ -48,6 +48,5 @@ async def to_code(config):
     for key in TEXT_SENSORS:
         if key in config:
             conf = config[key]
-            sens = cg.new_Pvariable(conf[CONF_ID])
-            await text_sensor.register_text_sensor(sens, conf)
+            sens = await text_sensor.new_text_sensor(conf)
             cg.add(getattr(hub, f"set_{key}_text_sensor")(sens))
